63445a18ab26ed0ff19295b95fe4ee5cf76fecd4
davit <davit.natenadze@gmail.com>
2022-12-24 15:19:01 +0100

    first commit

---
+++ UserDefaults/User.swift
@@ -0,0 +1,13 @@
+//
+//  User.swift
+//  UserDefaults
+//
+//  Created by Davit Natenadze on 23.12.22.
+//
+
+import Foundation
+
+struct User: Codable {
+    var name: String
+    var surname: String
+}
--- UserDefaults/ViewController.swift
+++ UserDefaults/ViewController.swift
@@ -8,12 +8,63 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    let userDefaults = UserDefaults.standard
+    
+    @IBOutlet weak var labelText: UILabel!
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        labelText.text = userDefaults.name
+//        let user = User(name: "lado", surname: "chania")
+//        userDefaults.userInfo = user
+        print(userDefaults.name)
+        userDefaults.removeObject(forKey: UserDefaults.UserDefKeys.name.rawValue)
+//        userDefaults.removeObject(forKey: userDefaults.name)
+//        print(userDefaults.userInfo?.name ?? "araferi")
+        
     }
 
-
+    @IBAction func savePressed(_ sender: UIButton) {
+        labelText.text = textField.text
+//        UserDefaults.standard.set(textField.text, forKey: "testText")
+        userDefaults.name = textField.text ?? "asd"
+        
+    }
+    
 }
 
+
+
+
+extension UserDefaults {
+    enum UserDefKeys: String {
+        case name, userInfo, ddd
+    }
+    
+    var name: String {
+        get {
+            string(forKey: UserDefKeys.name.rawValue) ?? "empty String"
+        }
+        set {
+            set(newValue, forKey: UserDefKeys.name.rawValue)
+        }
+    }
+    
+    var userInfo: User? {
+        get {
+            if let data = object(forKey: UserDefKeys.userInfo.rawValue)  as? Data {
+                let result = try? JSONDecoder().decode(User.self, from: data)
+                return result!
+            }
+            return nil
+        }
+        set {
+            let data = try? JSONEncoder().encode(newValue)
+            setValue(data, forKey: UserDefKeys.userInfo.rawValue)
+        }
+    }
+    
+}
